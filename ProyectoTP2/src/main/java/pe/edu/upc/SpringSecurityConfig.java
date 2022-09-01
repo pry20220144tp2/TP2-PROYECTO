283b2894158abb33af6edb7861cbcfa778fef2e0
pry20220144tp2 <pry20220144tp2@gmail.com>
2022-08-31 19:49:48 -0500

    holabola

--- ProyectoTP2/src/main/java/pe/edu/upc/SpringSecurityConfig.java
+++ ProyectoTP2/src/main/java/pe/edu/upc/SpringSecurityConfig.java
@@ -30,7 +30,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 				.authenticated().and().formLogin()
 				.loginPage("/login")
 				.permitAll()
-				.defaultSuccessUrl("/books/list")
+				.defaultSuccessUrl("/users/list")
 				.and().logout().permitAll().and()
 				.exceptionHandling().accessDeniedPage("/error");
 	}
